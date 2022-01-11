4120d8c0e4bc8b220c53382f8e2723d71aac4e29
Hilary Shaner <hilaryshaner@gmail.com>
2022-01-11 13:56:39 -0600

    first commit

---
+++ leonids-toys.js
@@ -0,0 +1,9 @@
+const train = {
+    id: 1,
+    name: "Wooden Railway System",
+    maker: "Right Track Toys",
+    material: "wood",
+    price: 24.99,
+    weightLbs: 3.6,
+    batteriesRequired: false
+}
